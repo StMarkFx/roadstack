12ffc0238c5c92d321afa2a47540cc2255348022
St. Mark <adebayomarkadedayo@gmail.com>
2025-03-15 23:49:20 +0100

    new files

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
     images: {
       domains: ["your-image-domain.com"],
     },
-    experimental: {
-      appDir: true,
-    },
     env: {
       NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
       NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
